f439e0d03541c9685c72076b7187f4c8e0b1d88a
etoal83 <koki.okutomi@gmail.com>
2020-09-25 00:53:45 +0900

    Add AppNavigator

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const HomeScreen = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Look around</Text>
+  </View>
+);
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
